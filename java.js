68da5c937ecb2b3a6e593dc0a1888fd76c31eca9
lephuc0492 <lephuc0492@gmail.com>
2019-05-28 23:24:27 +0700

    Update java.js

--- java.js
+++ java.js
@@ -7,16 +7,16 @@ function bai4_VietHoaKyTuDau(x)
 {
 	return x.charAt(0).toUpperCase() + x.slice(1).toLowerCase() //charAt: get ra ky tu dau, slice cat chuoi thanh 2 phan
 }
-function bai5_MinFromArray(x)
+function bai5_MinFromArray(x)					//Cach 2 dùng math.min.apply(null, x)
 {
-	var x = [];
-	return x.sort(function(a, b){return a-b})[0]; 
+	y = x.sort(function(a, b){return a-b})[0]; 
+	return y;
 
 }
 function bai6_InTenHocVienAbc(x)
 {
 	x = x.sort();
-document.write(x.join().replace(/,/g, "<br>")); //ham join bien 1 arr = 1 string
+document.write(x.join().replace(/,/g, "<br>")); //ham join bien 1 arr = 1 string Join("a") (Thêm ký tự a vào giua cac array)
 }
 function inTenHocVienAbcC2(x)
 {
@@ -84,6 +84,31 @@ function bai1_CoppyStringRepeat(x)
 // convert
 return y + x;
 }
+function bai1_CoppyStringRepeatC2(x)
+{
+		let y = "";
+for (var i = 0; i < 10; i++)
+{
+y += x + "-";
+}
+return	y.slice(0, y.length - 1);
+}
+function repeatstr(x,y,z)
+{
+	a = x + z;
+	b = a.repeat(y);
+	return b.slice(0, b.length -1);
+}
+function repeatstr2(x,y,z)
+{
+let a = [];
+for (var i = 1; i <= y; i++)
+{
+	a.push(x);
+}
+return a.join(z);
+}
+
 //factorial: Tức giai thừa
 function bai2_Factorial(x)				// đây cũng là một kiểu lặp đê giải bài giai thừa
 {
@@ -148,8 +173,55 @@ function bai6_SoChanSoLe()
 		}
 	}
 }
+function bai6_SoChanSoLe2()
+{
+	let x = [];
+	for (let i = 1; i <= 100; i++)
+	{	
+			document.write("<font color = red>" + i  + "</font>");
+			
+			document.write("<font color = blue>" + (i+=1)  + "</font>"); // Gán cho var i = 1 gia trị mới là i = i + 1
+			// lúc đấy hàm for biến thành for (let i = 2; i <=100; i++)
+	}
+	return x;
+}
+function thucHanh1(x)
+{
 
-
+for (let i = 0; i <= x; i++)
+{
+	if (i%3 == 0 && i%5 == 0)
+	{
+		document.write("<font color = red>" + i  + "</font>")
+	}
+	else if (i%5 == 0)
+	{
+		document.write("<font color = blue>" + i  + "</font>")
+	}
+	else if (i%3 == 0)
+	{
+		document.write("<font color = green>" + i  + "</font>")
+	}
+	else
+	{
+		document.write("<font color = orange>" + i  + "</font>")
+	}
+}
+}
+///////////////////////////
+function thucHanh2(x)
+{
+let a = "";
+for (let i = 0; i < x; i++)
+{
+	for (let j = 0; j < x; j++)
+	{
+		a += "*";
+	}
+		a += `<br>`;
+}
+	return a;
+}
 ////////////////////////////////////////ĐIỀU KIỆN - RẼ NHÁNH 1//////////////////////////////////
 function bai1_TestNumber(x,y)
 {
@@ -172,7 +244,7 @@ function bai1_TestNumber(x,y)
 
 function bai2_FactorialInteger(x)
 {
-	if (Number.isInteger(x) == true && x > 0) 				//hàm number is integer xác định xem số phải số nguyên dương khôg
+	if (Number.isInteger(x) == true && x >= 0) 				//hàm number is integer xác định xem số phải số nguyên dương khôg
 	{
 		document.write(bai2_Factorial(x));
 	}
@@ -182,9 +254,10 @@ function bai2_FactorialInteger(x)
 	}
 	else
 	{
+		return false;
 	}
 }
-function bai3_ArrayByNewArray(x)
+function bai3_ArrayToNewArray(x)
 {
 var y = x.filter(function(value, index, arr){					//filer trong javascripts cho phép lọc các giá trị
 	// trong mảng đó tạo ra 1 mạng mới theo điều kiện tương ứng, VD vakue % 2 == 0
@@ -233,7 +306,7 @@ function Bai1_GetDateMonthYearSeason()
 {
 	var x = new Date();				// bắt buộc phải là new Date không là ko gọi được các lệnh ngày tháng năm ... đâu
 	y = x.getDate() + "/" + (x.getMonth() + 1) + "/" + x.getFullYear();
-	if ((x.getMonth() + 1) >= 2 && (x.getMonth() + 1) <= 4)
+	if ((x.getMonth() + 1)  >= 2 && (x.getMonth() + 1) <= 4)
 	{
 			document.write("Mùa xuân " + y);
 	}
@@ -253,26 +326,26 @@ function Bai1_GetDateMonthYearSeason()
 
 function bai2_XepLoaiTheoDiem(x)
 {
-if (x >= 8.5 && x <= 10)
+if (x >= 8.5)
  {
  	console.log("Điểm A")
  }
- else if (x >= 7 && x < 8.5) 
- {
- 	console.log("Điểm B")
- }
- else if (x >= 5.5 && x < 7) 
- {
- 	console.log("Điểm C")
- }
- else if (x >= 4 && x < 5.5) 
- {
- 	console.log("Điểm D")
- }
- else if (x >= 0 && x < 4) 
- {
- 	console.log("Điểm E")
- }
+	 else if (x >= 7) 
+	 {
+	 	console.log("Điểm B")
+	 }
+	 else if (x >= 5.5) 
+	 {
+	 	console.log("Điểm C")
+	 }
+	 else if (x >= 4) 
+	 {
+	 	console.log("Điểm D")
+	 }
+	 else if (x >= 0) 
+	 {
+	 	console.log("Điểm E")
+	 }
  else
  {
  	console.log("Điền sai x")
@@ -310,3 +383,97 @@ for (var i = 0; i < x.length; i++)
 	}
 }
 }
+////////////////////////////////BT Vẽ Hình///////////////////
+function bai1_PrintTriangle(n)
+{
+	let str = "";
+	for(let i = 0; i < n; i++)
+	{
+		for(let j = 0; j < n; j++)
+		{
+			if (j == 1 && i == 0)
+			{
+			}
+			else if (j == 2 && i == 0){}
+			else if (j == 3 && i == 0){}
+			else if (j == 2 && i == 1){}
+			else if (j == 3 && i == 1){}
+			else if (j == 3 && i == 2){}				
+			else
+			{
+			str += document.write(" * ")
+			}
+		}
+			str += document.write("<br>")
+	}
+	return str;
+}
+function bai2_PrintEmptyRectangle(width, height)
+{
+	let str = "";
+	for(let i = 1; i <= height; i++)
+	{
+		for(let j = 1; j <= width; j++)
+		{
+			if (j == 2 && i == 2)
+			{
+				str += document.write('&ensp;')
+			}
+			else if (j == 3 && i == 2)
+			{	
+				str += document.write(`&emsp;`)
+			}
+			else if (j == 4 && i == 2)
+			{	
+				str += document.write(`&ensp;`)
+			}
+			
+			else
+			{
+			str += document.write(" * ")
+			}
+		}
+			str += document.write("<br>")
+	}
+	return str + '<pre>';
+}
+function bai3_PrintSpecialTriangle(height)
+{
+	let str = "";
+	for(let i = 1; i <= height; i++)
+	{
+		for(let j = 1; j <= 5; j++)
+		{
+			if (j == 1 && i == 2)
+			{
+				str += document.write('&ensp;')
+			}
+			else if (j == 5 && i == 2)
+			{	
+				str += document.write(`&emsp;`)
+			}
+			else if (j == 1 && i == 3)
+			{	
+				str += document.write(`&ensp;`)
+			}
+			else if (j == 2 && i == 3)
+			{	
+				str += document.write(`&ensp;&#160;`)
+			}
+			else if (j == 4 && i == 3)
+			{	
+				str += document.write(`&emsp;`)
+			}
+			else if (j == 5 && i == 3)
+			{	
+				str += document.write(`&emsp;`)
+			}		
+			else
+			{
+			str += document.write(" * ")
+			}
+		}
+			str += document.write("<br>")
+	}
+	return str + '</pre>';
+}	
