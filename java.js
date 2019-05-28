445f10a5c896eaba49c484844ea752d569ec0a05
lephuc0492 <49990927+lephuc0492@users.noreply.github.com>
2019-05-28 19:17:11 +0700

    Update java.js

--- java.js
+++ java.js
@@ -227,7 +227,7 @@ else
 function Bai1_GetDateMonthYear()
 {
 	var x = new Date();
-	document.write(x.getDate() + "/" + x.getMonth() + "/" + x.getFullYear())
+	document.write(x.getDate() + "/" + (x.getMonth() + 1) + "/" + x.getFullYear())
 }
 function Bai1_GetDateMonthYearSeason()
 {
